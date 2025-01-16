2c16559105f2639405778d43fb35c7302fab1c5d
nicopret1 <130547447+nicopret1@users.noreply.github.com>
2025-01-16 18:02:27 +0200

    Added state_class

--- victron_map.py
+++ victron_map.py
@@ -13,18 +13,21 @@ READ_PARAMETER_MAP = {
     },
     "BMS Battery Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Dc/Battery/Voltage",
         "module_type": "system"
     },
     "BMS Battery Load": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Dc/Battery/Power",
         "module_type": "system"
     },
     "Total PV Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Dc/Pv/Power",
         "module_type": "system"
@@ -137,7 +140,7 @@ READ_PARAMETER_MAP = {
         "module_type": "vebus",
         "map": {
             0: "OK",
-            2: "PhaseRotation"
+            1: "PhaseRotation warning"
         }
      },
     "Low Battery Alarm": {
@@ -218,174 +221,231 @@ READ_PARAMETER_MAP = {
      },
     "Load L1 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/Out/L1/P",
         "module_type": "vebus"
     },
+    "Load L1 Apparent Power": {
+        "device_class": "power",
+        "state_class": "measurement",
+        "unit": "VA",
+        "topic":  "Ac/Out/L1/S",
+        "module_type": "vebus"
+    },
     "Load L1 Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Ac/Out/L1/V",
         "module_type": "vebus"
     },
     "Load L1 Current": {
         "device_class": "current",
+        "state_class": "measurement",
         "unit": "A",
         "topic":  "Ac/Out/L1/I",
         "module_type": "vebus"
     },
     "Load L2 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/Out/L2/P",
         "module_type": "vebus"
     },
+    "Load L2 Apparent Power": {
+        "device_class": "power",
+        "state_class": "measurement",
+        "unit": "VA",
+        "topic":  "Ac/Out/L2/S",
+        "module_type": "vebus"
+    },
     "Load L2 Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Ac/Out/L2/V",
         "module_type": "vebus"
     },
     "Load L2 Current": {
         "device_class": "current",
+        "state_class": "measurement",
         "unit": "A",
         "topic":  "Ac/Out/L2/I",
         "module_type": "vebus"
     },
     "Load L3 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/Out/L3/P",
         "module_type": "vebus"
     },
+    "Load L3 Apparent Power": {
+        "device_class": "power",
+        "state_class": "measurement",
+        "unit": "VA",
+        "topic":  "Ac/Out/L3/S",
+        "module_type": "vebus"
+    },
     "Load L3 Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Ac/Out/L3/V",
         "module_type": "vebus"
     },
     "Load L3 Current": {
         "device_class": "current",
+        "state_class": "measurement",
         "unit": "A",
         "topic":  "Ac/Out/L3/I",
         "module_type": "vebus"
     },
     "Total Load Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/Out/P",
         "module_type": "vebus"
     },
+    "Total Apparant Load Power": {
+        "device_class": "power",
+        "state_class": "measurement",
+        "unit": "VA",
+        "topic":  "Ac/Out/S",
+        "module_type": "vebus"
+    },
     "Active Input L1 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/ActiveIn/L1/P",
         "module_type": "vebus"
     },
     "Active Input L2 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/ActiveIn/L2/P",
         "module_type": "vebus"
     },
     "Active Input L3 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/ActiveIn/L3/P",
         "module_type": "vebus"
     },
     "Grid Energy Import": {
         "device_class": "energy",
+        "state_class": "total_increasing",
         "unit": "kWh",
         "topic":  "Ac/Energy/Forward",
         "module_type": "grid"
     },
     "Grid Energy Export": {
         "device_class": "energy",
+        "state_class": "total_increasing",
         "unit": "kWh",
         "topic":  "Ac/Energy/Reverse",
         "module_type": "grid"
     },
     "Grid L1 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/L1/Power",
         "module_type": "grid"
     },
     "Grid L2 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/L2/Power",
         "module_type": "grid"
     },
     "Grid L3 Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/L3/Power",
         "module_type": "grid"
     },
     "Total Grid Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Ac/Power",
         "module_type": "grid"
     },
     "Grid L1 Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Ac/L1/Voltage",
         "module_type": "grid"
     },
     "Grid L2 Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Ac/L2/Voltage",
         "module_type": "grid"
     },
     "Grid L3 Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Ac/L3/Voltage",
         "module_type": "grid"
     },
     "Grid L1 Current": {
         "device_class": "current",
+        "state_class": "measurement",
         "unit": "A",
         "topic":  "Ac/L1/Current",
         "module_type": "grid"
     },
     "Grid L2 Current": {
         "device_class": "current",
+        "state_class": "measurement",
         "unit": "A",
         "topic":  "Ac/L2/Current",
         "module_type": "grid"
     },
     "Grid L3 Current": {
         "device_class": "current",
+        "state_class": "measurement",
         "unit": "A",
         "topic":  "Ac/L3/Current",
         "module_type": "grid"
     },
     "MPPT Voltage": {
         "device_class": "voltage",
+        "state_class": "measurement",
         "unit": "V",
         "topic":  "Dc/0/Voltage",
         "module_type": "solarcharger"
     },
     "MPPT Current": {
         "device_class": "current",
+        "state_class": "measurement",
         "unit": "A",
         "topic":  "Dc/0/Current",
         "module_type": "solarcharger"
     },
     "MPPT Power": {
         "device_class": "power",
+        "state_class": "measurement",
         "unit": "W",
         "topic":  "Yield/Power",
         "module_type": "solarcharger"
     },
-    "Total kWh produced": {
+    "Energy produced": {
         "device_class": "energy",
+        "state_class": "total_increasing",
         "unit": "kWh",
         "topic":  "Yield/System",
         "module_type": "solarcharger"
